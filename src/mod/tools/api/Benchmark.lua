21b72226eeb9b1f591978bd6d17dd29e40c5a2f7
Ruin0x11 <ipickering2@gmail.com>
2019-08-02 15:14:25 -0700

    Benchmark API

---
+++ src/mod/tools/api/Benchmark.lua
@@ -0,0 +1,26 @@
+local Stopwatch = require("api.Stopwatch")
+
+local Benchmark = {}
+
+function Benchmark.run(cb, runs, ...)
+   local sw = Stopwatch:new()
+   local indiv = {}
+   for i=1,runs do
+      cb(...)
+      indiv[#indiv + 1] = sw:measure()
+   end
+
+   local avg = 0
+   for _, v in ipairs(indiv) do
+      avg = avg + v
+   end
+
+   avg = avg / runs
+
+   return {
+      average = avg,
+      runs = indiv
+   }
+end
+
+return Benchmark
