46a2fc80e4db59c4cb4326a1ca12c30a76c758b7
Ruin0x11 <ipickering2@gmail.com>
2020-04-07 03:24:31 -0700

    Remove VS Code definition provider, as it's provided by EmmyLua

--- editor/vscode/src/extension.ts
+++ editor/vscode/src/extension.ts
@@ -1,27 +1,25 @@
 import * as vscode from 'vscode';
 import { hotloadThisFile, insertTemplate, describeAtPoint, insertRequireStatement, requireThisFileInRepl, launchGame, resetDrawLayers, sendSelectionToRepl, sendFileToRepl, insertId, searchDocumentation, makeScratchBuffer, createNewMod, insertEventHandler } from './commands';
-import { OpenNefiaDefinitionProvider } from './OpenNefiaDefinitionProvider';
 import { OpenNefiaHoverProvider } from './OpenNefiaHoverProvider';
 import { OpenNefiaColorProvider } from './OpenNefiaColorProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.hotloadThisFile', hotloadThisFile));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.insertTemplate', insertTemplate));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.launchGame', launchGame));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.describeAtPoint', describeAtPoint));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.insertRequireStatement', insertRequireStatement));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.requireThisFileInRepl', requireThisFileInRepl));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.resetDrawLayers', resetDrawLayers));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.sendSelectionToRepl', sendSelectionToRepl));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.sendFileToRepl', sendFileToRepl));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.insertId', insertId));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.searchDocumentation', searchDocumentation));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.makeScratchBuffer', makeScratchBuffer));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.createNewMod', createNewMod));
-    context.subscriptions.push(vscode.commands.registerCommand('opennefia.insertEventHandler', insertEventHandler));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.hotloadThisFile', hotloadThisFile));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.insertTemplate', insertTemplate));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.launchGame', launchGame));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.describeAtPoint', describeAtPoint));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.insertRequireStatement', insertRequireStatement));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.requireThisFileInRepl', requireThisFileInRepl));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.resetDrawLayers', resetDrawLayers));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.sendSelectionToRepl', sendSelectionToRepl));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.sendFileToRepl', sendFileToRepl));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.insertId', insertId));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.searchDocumentation', searchDocumentation));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.makeScratchBuffer', makeScratchBuffer));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.createNewMod', createNewMod));
+    context.subscriptions.push(vscode.commands.registerCommand('open-nefia.insertEventHandler', insertEventHandler));
 
     context.subscriptions.push(vscode.languages.registerColorProvider({ scheme: "file", language: "lua" }, new OpenNefiaColorProvider()));
-    context.subscriptions.push(vscode.languages.registerDefinitionProvider({ scheme: "file", language: "lua" }, new OpenNefiaDefinitionProvider()));
     context.subscriptions.push(vscode.languages.registerHoverProvider({ scheme: "file", language: "lua" }, new OpenNefiaHoverProvider()));
 
     vscode.window.showInformationMessage("OpenNefia extension loaded.");
