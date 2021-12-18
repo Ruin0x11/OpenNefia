4bb99f5780bd3ac29d6c4cd18bc7108e0bdbd9d1
Ruin0x11 <ipickering2@gmail.com>
2021-12-17 20:37:31 -0800

    yaml

--- src/scratch/2021-12-01_00-28-33.lua
+++ src/scratch/2021-12-01_00-28-33.lua
@@ -19,6 +19,12 @@ local function dotted(str)
    return ("%s.%s"):format(classify(mod_id), classify(data_id))
 end
 
+local function dottedEntity(str, ty)
+   local _, ty = ty:match("([^.]+)%.([^.]+)")
+   local mod_id, data_id = str:match("([^.]+)%.([^.]+)")
+   return ("%s.%s%s"):format(classify(mod_id), classify(ty), classify(data_id))
+end
+
 local function dotted_keys(t)
    return fun.iter(t):map(function(k, v) return dotted(k), v end):to_map()
 end
@@ -142,6 +148,9 @@ handlers["base.item"] = function(from, to)
    if (from.identify_difficulty or 0) ~= 0 then
       c.identifyDifficulty = from.identify_difficulty
    end
+   if from.originalnameref2 then
+      c.originalnameref2 = from.originalnameref2
+   end
 
    c = comp(to, "Tag")
    c.tags = {}
@@ -151,14 +160,19 @@ handlers["base.item"] = function(from, to)
    for _, tag in ipairs(from.tags or {}) do
       c.tags[#c.tags+1] = "tag:" .. tag
    end
+   if from.is_wishable == false then
+      c.tags[#c.tags+1] = "Elona.NoWish"
+   end
 
-   c = comp(to, "RandomGen")
-   c.tables.item = {
-      coefficient = from.coefficient or 400,
-      rarity = from.rarity or 100000
-   }
-   if (from.fltselect or 0) ~= 0 then
-      c.tables.item.fltselect = Enum.FltSelect:to_string(from.fltselect)
+   if (from.fltselect or 0) ~= 0 or (from.rarity or 0) ~= 0 then
+      c = comp(to, "RandomGen")
+      c.tables.item = {
+         coefficient = from.coefficient or 400,
+         rarity = from.rarity or 100000
+      }
+      if (from.fltselect or 0) ~= 0 then
+         c.tables.item.fltselect = Enum.FltSelect:to_string(from.fltselect)
+      end
    end
 
    if from.quality then
@@ -197,8 +211,13 @@ end
 
 local function transformMinimal(i)
    local t = automagic()
-   t.id = dotted(i._id)
-   t.type = capitalize(i._type:gsub("(.*)%.(.*)", "%2"))
+   if hspTypes[i._type] then
+      t.type = "Entity"
+      t.id = dottedEntity(i._id, i._type)
+   else
+      t.type = capitalize(i._type:gsub("(.*)%.(.*)", "%2"))
+      t.id = dotted(i._id)
+   end
    t.parent = hspTypes[i._type]
 
    if i.elona_id then
@@ -213,9 +232,10 @@ local function transformMinimal(i)
    return t
 end
 
-print(inspect())
 local datas = data["base.item"]:iter():into_sorted(sort):map(transformMinimal):to_list()
 print(lyaml.dump({ datas }))
+
+-- print(inspect(data["base.item"]:iter():filter(function(a) return a.fltselect > 0 and a.rarity == 0 end):to_list()))
 
 -- Local Variables:
 -- open-nefia-always-send-to-repl: t
