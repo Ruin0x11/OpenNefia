149af5f5ea610f150e0228671c2e2977021f6b0d
Ruin0x11 <ipickering2@gmail.com>
2021-12-21 04:57:35 -0800

    yaml

---
+++ src/scratch/2021-10-18_17-51-59.lua
@@ -0,0 +1,7 @@
+for _, entry in data["base.map_archetype"]:iter():filter(function(i) return i.on_generate_map == nil end) do
+   print(entry._id)
+end
+
+-- Local Variables:
+-- open-nefia-always-send-to-repl: t
+-- End:
